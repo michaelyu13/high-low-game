e14723bba26ee8e7bb003112b07290a94195636d
Michael Yu <michael@michaelyu.co.uk>
2023-10-29 16:16:36 +0000

    Refactor - Add tests folder to store test files

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,7 +1,7 @@
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
-import { render, fireEvent, screen } from '@testing-library/react';
 
-import Game from './pages/Game';
+import Game from '../pages/Game';
 
 describe('Game', () => {
     test('Play button is displayed and be enabled', () => {
--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: './src/setupTest.js',
+        setupFiles: './src/tests/setupTest.js',
     },
 });
